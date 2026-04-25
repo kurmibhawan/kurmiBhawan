b61f5ba0988b26cb6d75fc610a0bb19964df34e9
AbhishekVerma128 <vermaabhishek128@gmail.com>
2026-04-25 16:00:21 +0530

    footer fix

--- kurmiBhawan/includes/footer.php
+++ kurmiBhawan/includes/footer.php
@@ -1,13 +1,52 @@
-<footer class="footer">
-    <div class="footer-content">
-        <h3>U. P. Academy of Administration & Management</h3>
+<footer class="custom-footer">
 
-        <p>
-        This is the Official Website of Uttar Pradesh Academy of Administration & Management.
-        </p>
+    <div class="footer-container">
 
-        <p>Last Updated: 21 April 2026</p>
+        <!-- LEFT -->
+        <div class="footer-left">
+            <div class="footer-logo">
+                <img src="images/logo.png" alt="">
+                <div>
+                    <h2>U. P. Academy of Administration & Management</h2>
+                    <p>Government of Uttar Pradesh</p>
+                </div>
+            </div>
+
+            <p class="footer-desc">
+                This is the Official Website of Uttar Pradesh Academy of Administration & Management, Government of Uttar Pradesh, India. Content on this website is published and managed by Uttar Pradesh Academy of Administration & Management, Government of Uttar Pradesh.
+                For any query regarding this website, Please contact the <span>“Web Information Manager”</span>
+            </p>
+        </div>
+
+        <!-- RIGHT LINKS -->
+        <div class="footer-links">
+
+            <div>
+                <a href="#">Accessibility Statement</a>
+                <a href="#">Website Policies</a>
+                <a href="#">Security Policy</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Officer's Login</a>
+            </div>
+
+            <div>
+                <a href="#">Disclaimer</a>
+                <a href="#">Feedback</a>
+                <a href="#">Sitemap</a>
+                <a href="#">Archive</a>
+                <a href="#">Help</a>
+            </div>
+
+        </div>
+
     </div>
+
+    <!-- BOTTOM -->
+    <div class="footer-bottom">
+        <p>Last Update on Website : Saturday, 25 April, 2026</p>
+        <p>Visitor No. : 815413</p>
+    </div>
+
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
