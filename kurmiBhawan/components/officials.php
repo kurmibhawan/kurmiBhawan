518e1a71fc63f9079b5099695d05bbbe51d23618
kurmibhawan <kurmibhawan@gmail.com>
2026-05-09 11:00:42 +0530

    done

--- kurmiBhawan/components/officials.php
+++ kurmiBhawan/components/officials.php
@@ -1,9 +1,9 @@
 <div class="section-wrapper">
- 
+
   <!-- LEFT: Officials -->
   <div class="officials-panel">
     <div class="officials-grid">
- 
+
       <!-- Row 1: CM + Director General (larger/senior) -->
       <div class="row">
         <div class="card">
@@ -11,20 +11,28 @@
             <img src="assets/images/cm.jpg" alt="Yogi Aditya Nath"
               onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>YA</div>'">
           </div>
-          <h3>योगी आदित्यनाथ</h3>
-          <p>माननीय मुख्यमंत्री,<br>उत्तर प्रदेश</p>
+          <h3>श्री डी.एम. कटियार</h3>
+          <p>माननीय राष्ट्रीय अध्यक्ष,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
         </div>
- 
+        <div class="card">
+          <div class="photo-wrap">
+            <img src="assets/images/cm.jpg" alt="Yogi Aditya Nath"
+              onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>YA</div>'">
+          </div>
+          <h3>श्री हरीश वर्मा </h3>
+          <p>राष्ट्रीय महासचिव,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
+        </div>
+
         <div class="card">
           <div class="photo-wrap">
             <img src="assets/images/director.jpg" alt="M. Devaraj IAS"
               onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>MD</div>'">
           </div>
-          <h3>श्री एम. देवराज, आई.ए.एस</h3>
-          <p>प्रमुख सचिव एवं महानिदेशक</p>
+          <h3>श्री वीरेन्द्र सिंह</h3>
+          <p>राष्ट्रीय कोषाध्यक्ष,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
         </div>
       </div>
- 
+
       <!-- Row 2: Three officials -->
       <div class="row">
         <div class="card">
@@ -32,32 +40,32 @@
             <img src="assets/images/official3.jpg" alt="Sanjay Kumar IAS"
               onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>SK</div>'">
           </div>
-          <h3>श्री संजय कुमार, आई.ए.एस</h3>
-          <p>निदेशक</p>
+          <h3>डॉ. कुलदीप गंगवार</h3>
+          <p>प्रदेश अध्यक्ष,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
         </div>
- 
+
         <div class="card">
           <div class="photo-wrap">
             <img src="assets/images/official4.jpg" alt="Sunil Kumar Chaudhri IAS"
               onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>SC</div>'">
           </div>
-          <h3>श्री सुनील कुमार चौधरी, आई.ए.एस</h3>
-          <p>अपर निदेशक</p>
+          <h3>श्री मायाकांत कटियार उर्फ़ लाला भाई पटेल</h3>
+          <p>प्रदेश महासचिव,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
         </div>
- 
+
         <div class="card">
           <div class="photo-wrap">
             <img src="assets/images/official5.jpg" alt="Ratnesh Singh IAS"
               onerror="this.parentElement.innerHTML='<div class=\'avatar-placeholder\'>RS</div>'">
           </div>
-          <h3>श्री रत्नेश सिंह, आई0ए0एस0</h3>
-          <p>अपर निदेशक</p>
+          <h3>श्री आलेन्द्र सचान</h3>
+          <p>प्रदेश कोषाध्यक्ष,<br>कूर्मि क्षत्रिय सभा लखनऊ</p>
         </div>
       </div>
- 
+
     </div>
   </div>
- 
+
   <!-- RIGHT: News -->
   <div class="news-panel">
     <div class="news-header">
@@ -70,7 +78,7 @@
       </div>
     </div>
     <div class="news-body">
- 
+
       <div class="news-item">
         <div class="news-date"><span class="day">13</span><span class="month">Apr</span></div>
         <div class="news-content">
@@ -78,32 +86,35 @@
           <div class="meta"><span class="pdf-badge">PDF</span> 6742 KB | हिंदी | परिपत्र</div>
         </div>
       </div>
- 
+
       <div class="news-item">
         <div class="news-date"><span class="day">9</span><span class="month">Apr</span></div>
         <div class="news-content">
-          <div class="title">श्री रत्नेश सिंह, आईएएस (बैच 2017) ने दिनांक 09 अप्रैल, 2026 को अपर निदेशक के रूप में कार्यभार ग्रहण किया है।</div>
+          <div class="title">श्री रत्नेश सिंह, आईएएस (बैच 2017) ने दिनांक 09 अप्रैल, 2026 को अपर निदेशक के रूप में
+            कार्यभार ग्रहण किया है।</div>
           <div class="meta">🔊 | समाचार</div>
         </div>
       </div>
- 
+
       <div class="news-item">
         <div class="news-date"><span class="day">1</span><span class="month">Apr</span></div>
         <div class="news-content">
-          <div class="title">महानिदेशक उपाम, श्री एल. वेंकटेश्वर लू, आई.ए.एस. के सेवानिवृत्त होने के उपरांत, श्री एम. देवराज, आई.ए.एस. (प्रमुख सचिव, नियुक्ति एवं कार्मिक) द्वारा...</div>
+          <div class="title">महानिदेशक उपाम, श्री एल. वेंकटेश्वर लू, आई.ए.एस. के सेवानिवृत्त होने के उपरांत, श्री एम.
+            देवराज, आई.ए.एस. (प्रमुख सचिव, नियुक्ति एवं कार्मिक) द्वारा...</div>
           <div class="meta">🔊 | समाचार</div>
         </div>
       </div>
- 
+
       <div class="news-item">
         <div class="news-date"><span class="day">20</span><span class="month">Apr</span></div>
         <div class="news-content">
-          <div class="title">20-04-2026 को Mission Karmayogi के अंतर्गत iGOT पोर्टल पर उत्तर प्रदेश से कुल 21,90,150 उपयोगकर्ताओं का ऑनबोर्डिंग किया गया तथा कुल...</div>
+          <div class="title">20-04-2026 को Mission Karmayogi के अंतर्गत iGOT पोर्टल पर उत्तर प्रदेश से कुल 21,90,150
+            उपयोगकर्ताओं का ऑनबोर्डिंग किया गया तथा कुल...</div>
           <div class="meta">🔊 | समाचार</div>
         </div>
       </div>
- 
+
     </div>
   </div>
- 
+
 </div>
