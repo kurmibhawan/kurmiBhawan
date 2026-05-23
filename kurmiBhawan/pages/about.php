b1ab71d0f0555c48536abefa0cc7c5c947df4e33
kurmibhawan <kurmibhawan@gmail.com>
2026-05-23 09:46:10 +0530

    nav changes

--- kurmiBhawan/pages/about.php
+++ kurmiBhawan/pages/about.php
@@ -8,13 +8,11 @@
     <title>कूर्मि क्षत्रिय सभा, लखनऊ - आधिकारिक वेबसाइट</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/navbar.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <script defer src="../assets/js/navbar-breadcrumb.js"></script>
     <style>
         :root {
             --primary-color: #1a5276;
@@ -358,6 +356,7 @@
 </head>
 
 <body>
+    <?php include('../includes/header.php'); ?>
     <?php include('../components/navbar.php') ?>
     <div class="container my-4">
         <h2 class="page-title">हमारे बारे में</h2>
@@ -671,6 +670,7 @@
         </section>
     </div>
     <button id="backToTopBtn" title="Go to top">↑</button>
+    <?php include('../includes/footer.php'); ?>
 
     <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
     <script>
--- kurmiBhawan/pages/flag_song.php
+++ kurmiBhawan/pages/flag_song.php
@@ -8,6 +8,8 @@
     <title>जागो क़ुर्मी क्षत्रिय महान</title>
     <link rel="stylesheet" href="../assets/css/navbar.css">
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <script defer src="../assets/js/navbar-breadcrumb.js"></script>
 
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&family=Noto+Sans+Devanagari:wght@400;600;700&display=swap"
